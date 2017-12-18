c6846c5cb01fe4abf05924c8624e134bcdbb5722
sime1 <sime.pizzi@gmail.com>
2017-12-18 15:09:00 +0100

    added player functionalities

--- client.go
+++ client.go
@@ -25,7 +25,7 @@ func (client Client) getRequest(url string) *http.Request{
 }
 
 //GetMatchList retrieves a list of matches from the API.
-func (client Client) GetMatchList( page *Page, sort string, filter *Filter) ([]*Match, error){
+func (client Client) GetMatchList( page *Page, sort string, filter *MatchFilter) ([]*Match, error){
 	url := URL + "/matches" + createMatchListParams(page, sort, filter)
 	req := client.getRequest(url)
 	httpClient := &http.Client{}
@@ -47,7 +47,7 @@ func (client Client) GetMatchList( page *Page, sort string, filter *Filter) ([]*
 }
 
 //GetMatch lets you retrieve a single match from the API.
-//It takes the id as a parameter, and return the Match. In case of success, error is nil, else
+//It takes the id as a parameter, and returns the Match. In case of success, error is nil, else
 //it contains an error object with data about the error
 func (client Client) GetMatch(id string) (*Match, error) {
 	url := URL + "/matches/" + id
@@ -63,23 +63,65 @@ func (client Client) GetMatch(id string) (*Match, error) {
 	return match, nil
 }
 
-//Page lets you specify pagination options for the getMatchList() method
+func (client Client) GetPlayerList( filter *PlayerFilter) ([]*Player, error){
+	url := URL + "/players" + createPlayerListParams(filter)
+	req := client.getRequest(url)
+	httpClient := &http.Client{}
+	res,_ := httpClient.Do(req)
+	defer res.Body.Close()
+	players := make([]*Player, 0)
+	tempPlayers,err := jsonapi.UnmarshalManyPayload(res.Body, reflect.TypeOf(new(Player)));
+	if err != nil {
+		return nil, err
+	}
+	for _,p := range tempPlayers {
+		match, success := p.(*Player);
+		if !success {
+			return nil, errors.New("Typecast error: Player");
+		}
+		players = append(players, match)
+	}
+	return players, nil
+}
+
+//GetPlayer lets you retrieve a single player from the API.
+//It takes the id as a parameter, and returns the Player. In case of success, error is nil, else
+//it contains an error object with data about the error
+func (client Client) GetPlayer(id string) (*Player, error) {
+	url := URL + "/players/" + id
+	httpClient := &http.Client{}
+	req := client.getRequest(url)
+	res,_ := httpClient.Do(req)
+	defer res.Body.Close()
+	player := &Player{}
+	err := jsonapi.UnmarshalPayload(res.Body, player)
+	if err != nil {
+		return nil, err
+	}
+	return player, nil
+}
+
+//Page lets you specify pagination options for the GetMatchList() method
 type Page struct{
 	Offset *int
 	Limit *int
 }
 
-//Filter lets you decide how to filter the matches retrived by getMatchList()
-type Filter struct{
+//MatchFilter lets you decide how to filter the matches retrived by GetMatchList()
+type MatchFilter struct{
 	CreatedAtStart *string
 	CreatedAtEnd *string
-	PlayerIds *[]string
-	PlayerNames *[]string
-	TeamNames *[]string
-	GameMode *[]string
+	PlayerIds []string
 }
 
-func createMatchListParams(page *Page, sort string, filter *Filter) string {
+//PlayerFilter lets you decide which players GetPlayerList should retrieve
+type PlayerFilter struct {
+	PlayerNames []string
+	PlayerIds []string
+	SteamIds []string
+}
+
+func createMatchListParams(page *Page, sort string, filter *MatchFilter) string {
 	urlParams := make([]string, 0)
 	//page params
 	if page != nil{
@@ -102,17 +144,28 @@ func createMatchListParams(page *Page, sort string, filter *Filter) string {
 		if filter.CreatedAtEnd != nil{
 			urlParams = append(urlParams, "filter[createdAt-end]=" +  *filter.CreatedAtEnd)
 		}
-		if filter.PlayerIds != nil{
-			urlParams = append(urlParams, "filter[playerIds]=" + strings.Join(*filter.PlayerIds, ","))
+		if len(filter.PlayerIds) > 0 {
+			urlParams = append(urlParams, "filter[playerIds]=" + strings.Join(filter.PlayerIds, ","))
 		}
-		if filter.PlayerNames != nil{
-			urlParams = append(urlParams, "filter[playerNames]=" + strings.Join(*filter.PlayerNames, ","))
+	}
+	if len(urlParams) > 0{
+		return "?" + strings.Join(urlParams, "&")
+	}
+	return ""
+}
+
+func createPlayerListParams(filter *PlayerFilter) string {
+	urlParams := make([]string, 0)
+	//filter params
+	if filter != nil{
+		if len(filter.PlayerNames) > 0{
+			urlParams = append(urlParams, "filter[playerNames]="+ strings.Join(filter.PlayerNames, ","))
 		}
-		if filter.TeamNames != nil{
-			urlParams = append(urlParams, "filter[teamNames]=" + strings.Join(*filter.TeamNames, ","))
+		if len(filter.SteamIds) > 0{
+			urlParams = append(urlParams, "filter[steamIds]=" +  strings.Join(filter.SteamIds, ","))
 		}
-		if filter.GameMode != nil{
-			urlParams = append(urlParams, "filter[gameMode]=" + strings.Join(*filter.GameMode, ","))
+		if len(filter.PlayerIds) > 0{
+			urlParams = append(urlParams, "filter[playerIds]=" + strings.Join(filter.PlayerIds, ","))
 		}
 	}
 	if len(urlParams) > 0{
